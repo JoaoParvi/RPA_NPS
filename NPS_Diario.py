e16871548c9eb2eb395fe57ef3e8e5ff64974333
Joao Gabriel Ferreira Mendes <joao.mendes@parvi.com.br>
2025-07-09 14:34:01 -0300

    Atualizando codigo NPS diario para resolver erro

--- NPS_Diario.py
+++ NPS_Diario.py
@@ -11,11 +11,10 @@ from datetime import date
 from sqlalchemy import create_engine
 import urllib
 
-## Maneira de fazer o WebDriver Funcionar para nosso ambiente local ##
 print("Inicializando o navegador...")
 options = Options()
-options.binary_location = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
-caminho_driver = r"C:\Users\adm.luiz.vinicius\Downloads\chromedriver.exe"
+options.binary_location = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
+caminho_driver = r"C:\Users\joao.mendes\Documents\chromedriver-win64\chromedriver.exe"
 
 print("Inicializando o navegador manualmente...")
 navegador = webdriver.Chrome(
